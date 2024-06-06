821649aea4f2f29d23dd2742e62ad00e87b9ae4d
nick-leland <nicholasrleland@gmail.com>
2024-06-05 23:47:43 -0400

    Made lots of progress on the depth first search algorithm implimentation, uses randomized weights assigned to each location to determine sort order

--- gen.py
+++ gen.py
@@ -2,40 +2,53 @@ import numpy as np
 
 size = 10
 
+rng = np.random.default_rng(seed=223)
+
 x = np.zeros((size, size), dtype=np.int16)
-print(x)
-
-# Current Maze
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-# # # # # # # # # #
-
-# TODO Look more into the different types of rng within numpy
-rng = np.random.default_rng()
-
-directions = rng.integers(low=1, high=4, size=(size**2)).reshape(size, size)
-
-# This creates the associated directions for our maze
-# TODO Possible improvement to 8 cardinal directions rather then 4
-maze =  x + directions
-
-# North is 1
-# East is 2
-# South is 3
-# West is 4
-
-def passage(x, y, grid):
-
-    return
 
 
+# Initializes the random values for each maze position
+maze_values = rng.random((size, size))
+# print(maze_values)
+# print(maze_values[0][0])
 
+# Creates a list of all positions in the maze and sets their value equal to the random value
+maze = []
+zsize = size - 1
+for x in range(size):
+    for y in range(size):
 
+        # Determines the neighbors by the current x,y coordinate
+        neighbors = []
+        if y != 0:
+            neighbors.append(f"{x} {y-1}")
+        if y != zsize:
+            neighbors.append(f"{x} {y+1}")
+        if x != 0:
+            neighbors.append(f"{x-1} {y}")
+        if x != zsize:
+            neighbors.append(f"{x+1} {y}")
 
+        maze.append({f"{x} {y}" : [maze_values[0][0], neighbors]})
+
+# Verification print sequence
+# for position in maze:
+#     print(list(position.keys())[0], position[list(position.keys())[0]][1])
+
+def search(graph, node):
+    visited = []
+    stack = deque()
+
+    visited.append(node)
+    stack.append(node)
+
+    while stack:
+        s = stack.pop()
+        print(s, end=" ")
+
+    for n in reversed(graph[s]):
+        if n not in visited:
+            visited.append(n)
+            stack.append(n)
+
+search(maze, maze[0][list(maze[0].keys())[0]][1])
