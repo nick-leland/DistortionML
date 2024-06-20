9d3e7ec6b9b9f97724b8bc54ecd89b2d6dbcf089
nick-leland <nicholasrleland@gmail.com>
2024-06-19 21:49:04 -0400

    Finalized the database generation and updated the git ignore to automatically ignore the data folder.

--- db_gen.py
+++ db_gen.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 # Target image size should be around this? 
 
-def maze_grid_generation(size_x, size_y, walls, cell, max_y):
+def maze_grid_generation(size_x, size_y, walls, cell, max_y, counter):
     """Creates mazes and grids that both match a range of pixel prospects"""
     # Creates a folder for the type of data it's generating 
     # In this case Mazes and Grids which will use the same pixel information
@@ -31,7 +31,7 @@ def maze_grid_generation(size_x, size_y, walls, cell, max_y):
     maze = Image.fromarray(a)
     maze = maze.convert('RGB')
     os.makedirs("maze", exist_ok=True)
-    maze.save(f"maze/{size_x}_x_{size_y}_{walls}_{cell}m.jpg")
+    maze.save(f"maze/{size_x}_x_{size_y}_{walls}_{cell}_{counter}m.jpg")
     # maze.show()
 
 
@@ -45,41 +45,69 @@ if __name__ == "__main__":
     ratio_x, ratio_y = 16, 9
     target_x, target_y = 1280, 720
 
+
+    max_y = 734
+    min_y = 727
+    # Target is 720, range of + 14 pixels
+    tolerance = max_y - target_y
+    
     # Figure out max padding?
     # Maybe there is a better way to mathematically calculate this value
-
+    # This is used to find the min and max of y
     # max_y = 0
-    max_y = 734
-    """
-    print("Finding the max length for the y value of the pixel")
-    for _ in range(1, int((target_x / ratio_x)+1)):
-        pixel_combinations = [(9 * cell_size + (9+1) * wall_size) for cell_size in range(1, 100000) for wall_size in range(1, 100) if (16 * cell_size + (16 + 1) * wall_size) == 1280 and cell_size > wall_size]
-        if max(pixel_combinations) > max_y:
-            max_y = max(pixel_combinations)
-    print("Search completed")
-    print(f"max_y is {max_y}")
-    """
+    # min_y = float('inf')
+    # print("Finding the max length for the y value of the pixel")
+    # for _ in range(1, int((target_x / ratio_x)+1)):
+    #     pixel_combinations = [(9 * cell_size + (9+1) * wall_size) for cell_size in range(1, 100000) for wall_size in range(1, 100) if (16 * cell_size + (16 + 1) * wall_size) == 1280 and cell_size > wall_size]
+    #     if max(pixel_combinations) > max_y:
+    #         max_y = max(pixel_combinations)
+    #     if min(pixel_combinations) < min_y:
+    #         min_y = min(pixel_combinations)
+    # print("Search completed")
+    # print(f"max_y is {max_y}")
+    # print(f"min_y is {min_y}")
+    
 
-    for _ in range(int(input("How many mazes would you like to create"))):
+    def size(size, cell, wall):
+        result = (size * cell + (size + 1) * wall)
+        return result
+
+    runs = int(input("How many times would you like to generate a maze of each size?"))
+
+    total_cycles = target_x // ratio_x
+
+    for _ in range(total_cycles):
         size_x = 16 * (_+1)
-        if size_x > target_x:
-            pass
         size_y = int((size_x * 9) / 16)
-        pixel_combinations = [[cell_size, wall_size] for cell_size in range(1, 100000) for wall_size in range(1, 100) if (size_x * cell_size + (size_x + 1) * wall_size) == 1280 and cell_size > wall_size]
+        print(size_x, size_y)
 
+        print("running")
+        search = 1000
+        max_size = target_x + tolerance
+        pixel_combinations = [[cell_size, wall_size] for cell_size in range(1, search) for wall_size in range(1, search) if size(size_x, cell_size, wall_size) > target_x and size(size_x, cell_size, wall_size) < max_size and cell_size > wall_size]
+
+        print(pixel_combinations)
         counter = 0
+
         for combo in pixel_combinations:
             combinations.update({counter: [size_x, size_y, combo[1], combo[0]]})
             counter += 1
     # First need to locate the folder for where the data will go
 
+    print(combinations)
     count = 0
     r = len(combinations.keys())
-    for _ in combinations.keys():
-        print(f"Making a maze with the following:\nX Size = {combinations[_][0]}\nY Size = {combinations[_][1]}\nWall Size = {combinations[_][2]}\nCell Size = {combinations[_][3]}")
-        # I'm thinking of reformating this to instead be within the above loop.
-        maze_grid_generation(combinations[_][0], combinations[_][1], combinations[_][2], combinations[_][3], max_y)
-        print(f"""Generated Maze-Grid '{combinations[_][0]}_x_{combinations[_][1]}_{combinations[_][2]}_{combinations[_][3]}.jpg'""")
+
+    # For troubleshooting
+    y = list(combinations.keys())
+    y.reverse()
+    for _ in y:
+    # for _ in combinations.keys():
+        for x in range(runs):
+            print(f"Making a maze with the following:\nX Size = {combinations[_][0]}\nY Size = {combinations[_][1]}\nWall Size = {combinations[_][2]}\nCell Size = {combinations[_][3]}")
+            # I'm thinking of reformating this to instead be within the above loop.
+            maze_grid_generation(combinations[_][0], combinations[_][1], combinations[_][2], combinations[_][3], max_y, x)
+            print(f"""Generated Maze-Grid '{combinations[_][0]}_x_{combinations[_][1]}_{combinations[_][2]}_{combinations[_][3]}.jpg'""")
 
 
 
